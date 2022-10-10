c53099d8ee3a2df6d614d9ee8a2469b2552f548b
Jordan <66105915+JordanC28@users.noreply.github.com>
2022-10-10 11:48:13 -0500

    Update facepalm.js

--- src/commands/images/facepalm.js
+++ src/commands/images/facepalm.js
@@ -10,7 +10,7 @@ module.exports = async (client, interaction, args) => {
     const image = await Canvas.facepalm(userAvatar);
     let attach = new Discord.MessageAttachment(image, "facepalm.png");
 
-    interaction.editreply({ files: [attach] })
+    interaction.editReply({ files: [attach] })
 }
 
  

1592abcb27af8db0fdf1435c0224dffbb71e21e8
Jordan <66105915+JordanC28@users.noreply.github.com>
2022-10-10 11:49:25 -0500

    Update glass.js

--- src/commands/images/glass.js
+++ src/commands/images/glass.js
@@ -5,5 +5,5 @@ module.exports = async (client, interaction, args) => {
     let link = `https://some-random-api.ml/canvas/glass/?avatar=${interaction.user.avatarURL({ size: 1024, format: 'png' })}`
     const attachment = new Discord.MessageAttachment(link, 'glass.png');
 
-    interaction.editreply({ files: [attachment] })
+    interaction.editReply({ files: [attachment] })
 };

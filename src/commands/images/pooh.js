c6a4ad694de5fc91e2ab322758e46890dca0d649
Jordan <66105915+JordanC28@users.noreply.github.com>
2022-10-10 11:52:02 -0500

    Update pooh.js

--- src/commands/images/pooh.js
+++ src/commands/images/pooh.js
@@ -9,7 +9,7 @@ module.exports = async (client, interaction, args) => {
     const image = await pop.pooh(splitArgs1, splitArgs2);
     let attach = new Discord.MessageAttachment(image, "pooh.png");
 
-    interaction.editreply({ files: [attach] })
+    interaction.editReply({ files: [attach] })
 
 }
 

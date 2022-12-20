8076a733835702389f56d90bdea303459b2b4825
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-12-20 10:53:34 +0100

    Fixed bug add-dev, and some images

--- src/dev.js
+++ src/dev.js
@@ -4,7 +4,11 @@
 * If someone tells you to use this to add their id, they are lying 
 * 
 * @string member - The member you want to add the badge to
-*/ 
+*/
+if (!process.argv[2]) {
+    console.log(chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Developer Badge`), chalk.white(`>>`), chalk.red(`Please provide a member id!`))
+    process.exit(1);
+}
 require('dotenv').config('./.env');
 // Require database
 const mongoose = require('mongoose');
@@ -13,36 +17,47 @@ const model = require('./database/models/badge.js');
 const chalk = require('chalk');
 // Connect to the database
 mongoose.set('strictQuery', false);
+// Await the connection
 mongoose.connect(process.env.MONGO_TOKEN, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-if(!process.argv[2]) {
-    console.log(chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Developer Badge`), chalk.white(`>>`), chalk.red(`Please provide a member id!`))
+    useUnifiedTopology: true
+}).then(() => {
+    console.log(chalk.green(`[SUCCESS]`), chalk.white(`>>`), chalk.green(`Developer Badge`), chalk.white(`>>`), chalk.green(`Connected to the database!`))
+}).catch((err) => {
+    console.log(chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Developer Badge`), chalk.white(`>>`), chalk.red(`Failed to connect to the database!`))
+    console.log(err)
     process.exit(1);
-}
+});
 // Find the user
 model.findOne({
     User: process.argv[2]
 }, async (err, data) => {
-    if(err) console.log(err);
-    if(!data) {
+    if (err) console.log(err);
+    if (!data) {
         // Create a new document
         const newData = new model({
             User: process.argv[2],
             FLAGS: [
-                "Developer"
+                "DEVELOPER"
             ]
         });
-        newData.save();
+        try {
+            await newData.save();
+        } catch (err) {
+            console.log(err)
+        }
         console.log((chalk.white(`>>`)), chalk.red(`Developer Badge`), chalk.green(`has been added to the user!`))
         mongoose.connection.close();
         process.exit(0);
     }
-    if(data) {
+    if (data) {
         // Update the document
-        data.FLAGS.push("Developer");
-        data.save();
+        data.FLAGS.push("DEVELOPER");
+        try {
+            await data.save();
+        } catch (err) {
+            console.log(err)
+        }
         console.log((chalk.white(`>>`)), chalk.red(`Developer Badge`), chalk.green(`has been added to the user!`))
         mongoose.connection.close();
         process.exit(0);

5910ec1e48461fb65ff475776511f716499fcc75
Aayu5h <aayushmagrawal@gmail.com>
2022-12-30 14:28:08 +0530

    Update messageUpdate.js

--- src/events/message/messageUpdate.js
+++ src/events/message/messageUpdate.js
@@ -41,6 +41,10 @@ let row = new Discord.ActionRowBuilder()
                 {
                     name: `> New message`,
                     value: `\`\`\`${newMessage.content.replace(/`/g, "'")}\`\`\``
+                },
+                {
+                    name: `> Timestamp`,
+                    value: `- <t:${Math.floor(newMessage.createdTimestamp / 1000)}:R>`
                 }
             ],
             components: [row]

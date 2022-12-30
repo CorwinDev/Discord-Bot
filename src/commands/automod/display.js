4d83f5ef364b7d4347af9a159b2f038c2ccde872
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2022-12-30 14:47:36 +0100

    Update display.js

--- src/commands/automod/display.js
+++ src/commands/automod/display.js
@@ -7,14 +7,14 @@ module.exports = async (client, interaction, args) => {
     Schema.findOne({ Guild: interaction.guild.id }, async (err, data) => {
         if (data && data.Words.length > 0) {
             client.embed({
-                title: "<:uo_BotEvent:1015565719330627584>・Blacklisted words",
+                title: "<:uo_BotEvent:1015565719330627584>・Mots dans la liste noire",
                 desc: data.Words.join(", "),
                 type: 'editreply'
             }, interaction)
         }
         else {
             client.errNormal({
-                error: `This guild has not data!`,
+                error: `Ce serveur ne possède pas de données!`,
                 type: 'editreply'
             }, interaction);
         }

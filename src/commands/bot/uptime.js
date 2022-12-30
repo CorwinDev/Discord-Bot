7fef640f16edd5412e4c20fa95d8ac207c8d931c
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2022-12-30 18:22:53 +0100

    Update uptime.js

--- src/commands/bot/uptime.js
+++ src/commands/bot/uptime.js
@@ -3,12 +3,12 @@ const moment = require("moment");
 require("moment-duration-format");
 
 module.exports = async (client, interaction, args) => {
-    const duration = moment.duration(client.uptime).format("\`D\` [days], \`H\` [hrs], \`m\` [mins], \`s\` [secs]");
+    const duration = moment.duration(client.uptime).format("\`D\` [j], \`H\` [h], \`m\` [m], \`s\` [s]");
     const upvalue = (Date.now() / 1000 - client.uptime / 1000).toFixed(0);
 
     client.embed({
         title: `${client.emotes.normal.arrowUp}・Uptime`,
-        desc: `See the uptime of Bot`,
+        desc: `Voir le uptime du Bot`,
         fields: [
             {
                 name: "<:uo_clock:1015551740281622538> ┇ Uptime",
@@ -16,7 +16,7 @@ module.exports = async (client, interaction, args) => {
                 inline: true
             },
             {
-                name: "<:uo_clock:1015551740281622538> ┇ Up Since",
+                name: "<:uo_clock:1015551740281622538> ┇ Up depuis",
                 value: `<t:${upvalue}>`,
                 inline: true
             }

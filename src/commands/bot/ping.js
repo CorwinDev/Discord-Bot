45c39e84853d5ef88a4d4992399452fe8ffd8b81
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2022-12-30 18:21:45 +0100

    Update ping.js

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -16,20 +16,20 @@ module.exports = async (client, interaction, args) => {
 
             client.embed({
                 title: `${client.emotes.normal.pong}・Pong`,
-                desc: `Check out how fast our bot is`,
+                desc: `Vérifier la rapidité du bot`,
                 fields: [
                     {
-                        name: "<:discord_bot:1012038552521031703> ┆ Bot latency",
+                        name: "<:discord_bot:1012038552521031703> ┆ Bot latence",
                         value: `${ping}ms (${pingSeconds}s)`,
                         inline: true,
                     },
                     {
-                        name: "<:blue_hammers:1012018248163786763> ┆ API Latency",
+                        name: "<:blue_hammers:1012018248163786763> ┆ API Latence",
                         value: `${client.ws.ping}ms (${apiSeconds}s)`,
                         inline: true,
                     },
                     {
-                        name: "<:to_space:1012038751729491968> ┆ Database Latency",
+                        name: "<:to_space:1012038751729491968> ┆ Database Latence",
                         value: `${result.ok}ms (${mongooseSeconds}s)`,
                         inline: true,
                     }

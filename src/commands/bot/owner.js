ccd2cfaeae40ba5ebf8e560c20d2883404227c3b
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2022-12-30 18:20:40 +0100

    Update owner.js

--- src/commands/bot/owner.js
+++ src/commands/bot/owner.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js');
 
 module.exports = async (client, interaction, args) => {
     client.embed({
-        title: `・Owner information`,
+        title: `・Information propriétaire`,
         desc: `____________________________`,
         thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
         fields: [{
-            name: "<:blue_crown:1012017210992115812> ┆ Owner name",
-            value: `Uo`,
+            name: "<:blue_crown:1012017210992115812> ┆ Propriétaire",
+            value: `Pierre`,
             inline: true,
         },
         {
             name: "<:discord:1012017257158824027> ┆ Discord tag",
-            value: `Uo#1428`,
+            value: `<@87107972676751360>`,
             inline: true,
         }],
         type: 'editreply'

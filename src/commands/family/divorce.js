907b0551d9f94aa14975bb8ded469cf0de72c74e
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2023-01-11 20:21:50 +0100

    Update divorce.js

--- src/commands/family/divorce.js
+++ src/commands/family/divorce.js
@@ -29,7 +29,7 @@ module.exports = async (client, interaction, args) => {
         data.save();
 
         client.embed({ 
-            title: `👰・Divorcé`, 
+            title: `👰・Divorcés`, 
             desc: `${author} et ${target} viennent de divorcer`, 
             type: 'editreply' 
         }, interaction);

6b00f3dfa6e32086f832cdcbebd0dfa6734d634c
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2023-01-11 20:04:45 +0100

    Update errorCreate.js

--- src/events/client/errorCreate.js
+++ src/events/client/errorCreate.js
@@ -14,59 +14,59 @@ module.exports = (client, err, command, interaction) => {
 
     let embed = new Discord.MessageEmbed()
         .setTitle(`🚨・${password}`)
-        .addField("✅┇Guild", `${interaction.guild.name} (${interaction.guild.id})`)
-        .addField(`💻┇Command`, `${command}`)
-        .addField(`💬┇Error`, `\`\`\`${err}\`\`\``)
-        .addField(`📃┇Stack error`, `\`\`\`${err.stack.substr(0, 1018)}\`\`\``)
+        .addField("✅┇Serveur", `${interaction.guild.name} (${interaction.guild.id})`)
+        .addField(`💻┇Commande`, `${command}`)
+        .addField(`💬┇Erreur`, `\`\`\`${err}\`\`\``)
+        .addField(`📃┇Erreur stack`, `\`\`\`${err.stack.substr(0, 1018)}\`\`\``)
         .setColor(client.config.colors.normal)
     errorlog.send({
-        username: `Bot errors`,
+        username: `Erreurs du bot`,
         embeds: [embed],
 
     }).catch(error => { console.log(error) })
 
-    let row = new Discord.MessageActionRow()
+    /*let row = new Discord.MessageActionRow()
         .addComponents(
             new Discord.MessageButton()
                 .setLabel("Support server")
                 .setURL(client.config.discord.serverInvite)
                 .setStyle("LINK"),
-        );
+        );*/
 
     client.embed({
-        title: `${client.emotes.normal.error}・Error`,
-        desc: `There was an error executing this command`,
+        title: `${client.emotes.normal.error}・Erreur`,
+        desc: `Il y a eu une erreur en executant cette commande`,
         color: client.config.colors.error,
         fields: [
             {
-                name: `Error code`,
+                name: `Code d'erreur`,
                 value: `\`${password}\``,
                 inline: true,
-            },
+            }/*,
             {
                 name: `What now?`,
                 value: `You can contact the developers by joining the support server`,
                 inline: true,
-            }
+            }*/
         ],
         components: [row],
         type: 'editreply'
     }, interaction).catch(() => {
         client.embed({
             title: `${client.emotes.normal.error}・Error`,
-            desc: `There was an error executing this command`,
+            desc: `Il y a eu une erreur en executant cette commande`,
             color: client.config.colors.error,
             fields: [
                 {
-                    name: `Error code`,
+                    name: `Code d'erreur`,
                     value: `\`${password}\``,
                     inline: true,
-                },
+                }/*,
                 {
                     name: `What now?`,
                     value: `You can contact the developers by joining the support server`,
                     inline: true,
-                }
+                }*/
             ],
             components: [row],
             type: 'reply'

14e31176976137827e3d3a80241204574a961e19
pierrequiroul <pierrelac@hotmail.be>
2023-01-15 23:11:14 +0100

    Status

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,36 +2,36 @@ const Discord = require('discord.js');
 const chalk = require('chalk');
 
 module.exports = async (client) => {
-  const activities = [
+  const activities2 = [
         { name: `${client.guilds.cache.size} Servers`, type: 2 }, // LISTENING
         { name: `${client.channels.cache.size} Channels`, type: 0 }, // PLAYING
         { name: `${client.users.cache.size} Users`, type: 3 }, // WATCHING
         { name: `si Eliott fait pas bêtises`, type: 3 },
+    ];
+const activities = [
+        { name: `League of Legends`, type: 0 },
+        { name: `vec ta mère`, type: 0 },
+        { name: `si aucune rebellion ne se forme`, type: 3 },
+        { name: `le crépitement de la friture`, type: 2 },
+        { name: `si Eliott fait pas de bêtises`, type: 3 },
+        { name: `si ça joue à LoL`, type: 3 },
     ];
     const status = [
         'online',
         'dnd',
         'idle'
     ];
-   /* let i = 0;
+    let i = 0;
     setInterval(() => {
         if(i >= activities.length) i = 0
         client.user.setActivity(activities[i])
         i++;
     }, 5000);
-  
-    let s = 0;
-    setInterval(() => {
-        if(s >= activities.length) s = 0
-        
-        s++;
-    }, 30000);
-  */
+    
     client.user.setStatus(status[0])
-    client.user.setActivity(activities[3])
   
     client.player.init(client.user.id);    
-  console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), chalk.red(`${client.user.username}`), chalk.green(`is ready!`))
+  console.log(chalk.blue(chalk.bold(`Systeme`)), (chalk.white(`>>`)), chalk.red(`${client.user.username}`), chalk.green(`est prêt !`))
 }
 
  

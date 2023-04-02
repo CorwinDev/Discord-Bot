eca04ce48cc91ba34b2268354d598a315f298818
pierrequiroul <pierrelac@hotmail.be>
2023-04-02 22:56:54 +0200

    config

---
+++ src/config/changelogs.js
@@ -0,0 +1,28 @@
+module.exports = {
+    release: "11-1-2022",
+    bugs: [
+        "Better music system",
+        "Better bot setups",
+        "Giveaways fixed",
+        "A new invite tracker system",
+        "Channel logs bug fixed",
+        "Better error handling for commands"
+    ],
+    features: [
+        "Bot completely in slash commands",
+        "New Activities",
+        "New server stats",
+        "Auto setups",
+        "Scheduled Events logs",
+        "Mute command is now timeout command",
+        "Embed layout improvements",
+        "A advanced embed builder",
+        "Removed snipe commands",
+        "Unnecessary commands removed",
+        "Some commands rearranged",
+        "**Bot 1 year pack** available for **10 Dcredits**",
+        "Better error handling for commands"
+    ],
+};
+
+ 

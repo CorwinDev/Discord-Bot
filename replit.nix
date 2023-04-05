37d10a6730704a3779671db15fe5a4de5f4e00f1
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2023-04-05 20:02:11 +0000

    replit settings

--- replit.nix
+++ replit.nix
@@ -1,9 +1,13 @@
 { pkgs }: {
 	deps = [
 		pkgs.ffmpeg.bin
-  pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.yarn
-        pkgs.replitPackages.jest
+    pkgs.nodejs-18_x
+    pkgs.libuuid
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
 	];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
 }

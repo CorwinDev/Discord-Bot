5a83a6423408ef96a868697eefa396000e1e9563
pierrequiroul <pierrelac@hotmail.be>
2023-05-25 23:38:30 +0200

    tes

--- src/handlers/response/watchparty.js
+++ src/handlers/response/watchparty.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const https = require('https');
-import { app } from '../../index.js';
-app.listen(3000);
 
 module.exports = async (client) => {
     const regex = {

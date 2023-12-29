334db7cd2506f65028d29abffbb3848457622962
pierrequiroul <35348696+pierrequiroul@users.noreply.github.com>
2023-12-29 18:37:14 +0100

    Freebies Unreal (#43)
    * première passe
    
    * Finishing feature + added command to call

---
+++ src/interactions/Command/freebies.js
@@ -0,0 +1,28 @@
+const { CommandInteraction, Client } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('freebies')
+        .setDescription('Afficher les freebies')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('unreal')
+                .setDescription('Afficher les Freebies du mois du Marketplace')
+        )
+    ,
+
+    /** 
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+
+    run: async (client, interaction, args) => {
+        await interaction.deferReply({ fetchReply: true });
+        client.loadSubcommands(client, interaction, args);
+    },
+};
+
+ 

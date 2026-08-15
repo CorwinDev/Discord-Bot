e65af3c16ef46ba67edc613a4a489c5da64e9cd4
Corwin <info@corwindev.nl>
2026-08-15 20:48:40 +0200

    fix: error bug

--- src/events/music/error.js
+++ src/events/music/error.js
@@ -1,6 +1,18 @@
-const Discord = require('discord.js');
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 module.exports = (client, node, error) => {
-    console.log(chalk.red(chalk.bold(`ERROR`)), (chalk.white(`>>`)), chalk.white(`Node`), chalk.red(`${node}`), chalk.white(`had an error:`), chalk.red(`${error.message}`))
+  const errorMessage =
+    error instanceof Error
+      ? error.message
+      : String(error ?? "Unknown Shoukaku error");
+  const nodeName = node?.name ?? node ?? "Unknown node";
+
+  console.log(
+    chalk.red(chalk.bold(`ERROR`)),
+    chalk.white(`>>`),
+    chalk.white(`Node`),
+    chalk.red(`${nodeName}`),
+    chalk.white(`had an error:`),
+    chalk.red(errorMessage),
+  );
 };
